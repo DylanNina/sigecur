069e9f445ca25bee4a81dc15455023f02a1e7fbd
Oliver Laura <laurasantallaolivermauro@gmail.com>
2026-03-13 16:08:44 -0400

    commit4 oliver

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="content-wrapper">
+
+    <section class="content-header">
+        <h1>Dashboard</h1>
+    </section>
+
+    <section class="content">
+        Bienvenido al sistema SIGECUR
+    </section>
+
+</div>
+
+@endsection
